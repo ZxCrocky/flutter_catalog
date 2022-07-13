d56d0552d83b7c9b5da45b0f5e571401530ec42d
ZxCrocky <f20190133@goa.bits-pilani.ac.in>
2022-07-13 12:53:59 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyWidget());
@@ -14,15 +15,18 @@ class MyWidget extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
-        "/":(context)=>LoginPage(),
-        "/home" :(context) => HomePage(),
-        "/login":(context)=>LoginPage(),
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -3,14 +3,56 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login page",style: TextStyle(
-          fontSize: 30,
-          color: Colors.blue,
-          fontStyle: FontStyle.italic,
-        ) ,),
-      ),
+    return Material(
+      color: Colors.white,
+      child: Column(children: [
+        Image.asset(
+          "assets/images/undraw_Mobile_login_re_9ntv.png",
+          fit: BoxFit.cover,
+        ),
+        const SizedBox(
+          width: 10, //you may use this or padding by wrapping around text
+          height: 10, // used this to put space between the image and text
+        ),
+        const Text(
+          "Welcome",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+        ),
+        const SizedBox(
+          width: 10,
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Enter your username",
+                  labelText: "Username",
+                ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  hintText: "Enter the password",
+                  labelText: "Password",
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+                height: 10,
+              ),
+              ElevatedButton(
+                child: const Text("Login"),
+                onPressed: () {
+                  print("hemlo ");
+                },
+              )
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
