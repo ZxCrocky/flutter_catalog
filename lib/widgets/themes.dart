bd550d2a9d70fee2b533092da36a334620fdfdc1
ZxCrocky <f20190133@goa.bits-pilani.ac.in>
2022-07-14 13:28:48 +0530

    Day 10 completed

---
+++ lib/widgets/themes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+    primarySwatch: Colors.lightGreen,
+    fontFamily: GoogleFonts.lato().fontFamily,
+    appBarTheme: 
+    const AppBarTheme(
+    color: 
+      Colors.white,
+      elevation: 0.0,
+      iconTheme: 
+        IconThemeData(color: Colors.black),
+        titleTextStyle:
+          TextStyle(
+          color:
+          Colors.black, 
+          fontSize: 18, 
+          fontWeight: FontWeight.bold
+          ),      
+    )
+  );
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+      brightness: Brightness.dark,
+  );
+}
